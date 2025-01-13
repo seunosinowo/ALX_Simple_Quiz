ec5fe6547a1488051a763dc5fe16dea83e3ccc12
seunosinowo <oluwaseunpaul98@gmail.com>
2025-01-13 13:06:24 +0100

    Added event

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
     if (userAnswer === correctAnswer) {
-        document.getElementById('feedback').textContent = "Correct! Well done";
+        document.getElementById('feedback').textContent = "Correct! Well done.";
     } else {
         document.getElementById('feedback').textContent = "That's incorrect. Try again";
     }
