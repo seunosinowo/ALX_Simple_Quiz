caad08636ae0e529d071be18a929126dd670fcfb
seunosinowo <oluwaseunpaul98@gmail.com>
2025-01-13 13:08:54 +0100

    Element id solved

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ function checkAnswer() {
     if (userAnswer === correctAnswer) {
         document.getElementById('feedback').textContent = "Correct! Well done.";
     } else {
-        document.getElementById('feedback').textContent = "That's incorrect. Try again";
+        document.getElementById('feedback').textContent = "That's incorrect. Try again!";
     }
 }
 
